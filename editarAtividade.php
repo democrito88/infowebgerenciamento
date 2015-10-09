3bcfd1ad21423be8818ea35fc604eba70f34f61c
erickcesar <erickcesar09@hotmail.com>
2015-10-09 09:37:56 -0300

    09/10/2015   9:37

--- editarAtividade.php
+++ editarAtividade.php
@@ -11,7 +11,7 @@ while($processos = mysql_fetch_object($resultado)){
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<html xmlns="http://www.w3.org/1999/xhtml"style="padding-right: 400px;"><head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>INFO WEB ADMINISTRATOR</title>
 <?php cabecalho();?>
