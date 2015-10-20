0edcf6c63ed4c1af7076fb8b7cc923baddc0aed1
democrito88 <d_who@hotmail.com>
2015-10-20 10:41:13 -0200

    Realizando commit 09:40 20/10/2015 - por Demócrito

--- php/email.php
+++ php/email.php
@@ -1,6 +1,6 @@
 <?php
-//include "conexao.php";
-conexaoInfoweb();
+include "conexao.php";
+$link = conexaoInfoweb();
 
 $nomeUsuario = '';
 $subject = "INFOWEBGERENCIAMENTO - Notificação";
@@ -10,13 +10,13 @@ $from = "";
 $headers = "From: naoresponda@olinda.pe.gov.br";
 
 $consulta = "SELECT idUsuario, nomeUsuario FROM usuario WHERE DATEDIFF(ultimoAcesso, CURDATE()) < 7";
-$usuarios = mysql_query($consulta);
+$usuarios = mysql_query($consulta, $link) or die(mysql_error());
 
 while($usuario = mysql_fetch_object($usuarios)){
     //seleciona as atividades que estão atrasadas de um usuário específico
     $query = "SELECT a.nomeAtividade, a.dataInicial, a.dataFinal, us.email FROM atividade a INNER JOIN usuario us WHERE DATEDIFF(a.dataFinal, CURDATE()) < 7 AND a.status = 3 AND us.idUsuario = ".$usuario->idUsuario;
 
-    $resultado = mysql_query($query);
+    $resultado = mysql_query($query) or die(mysql_error());
     
     $message = "Olá, ".$usuario->nomeUsuario.".\n
     \tEsta mensagem está sendo enviada para notificá-lo(a) que no sistema infowebgerenciamento constam como atrasadas a(s) seguinte(s) atividade(s):";
@@ -35,9 +35,17 @@ while($usuario = mysql_fetch_object($usuarios)){
     \nInfowebgerenciamento - Sistema de gerenciamento de atividades
     \nCoordenadoria de informática - PMO";
 
+    mysql_close($link);
+    
     //nescessário colocar um e-mail real
     mail($to,$subject,$message,$headers);
     echo $to."<br>";
     echo $headers;
+    
+    $myfile = fopen("log_de_emails.txt", "w") or die("Unable to open file!");
+    $message = "\n----------------------------------------------".  date("d-m-Y")."\n".$message."\n\n";
+    echo $message;
+    fwrite($myfile, $message);
+    fclose($myfile);
 }
 ?>
